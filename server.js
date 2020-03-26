5492542345a4d16739da2f65388f89ba0f686806
krishan93985 <krisharush93985@gmail.com>
2020-03-26 08:31:56 +0530

    connecting database

--- server.js
+++ server.js
@@ -11,10 +11,8 @@ app = express();
 const db = knex({
     client: 'pg',
     connection: {
-      host : '127.0.0.1',
-      user : 'postgres',
-      password : 'learn',
-      database : 'smartbrain'
+      connectionString : process.env.DATABASE_URL,
+      ssl:true
     }
   });
 
